17fa1a7133d20917181c84a8dad769bad0228155
Chez0r <walop9606@gmail.com>
2019-05-26 19:06:34 +0300

    Server window update

--- src/WindowServer.java
+++ src/WindowServer.java
@@ -8,31 +8,36 @@ import java.sql.SQLException;
 
 public class WindowServer {
 
-
-
     public WindowServer () {
-
-
 
         JFrame MainWindowAuth = new JFrame("Сервер");
         MainWindowAuth.setLocationRelativeTo(null);
         MainWindowAuth.setSize(350, 150);
         MainWindowAuth.setVisible(true);
         MainWindowAuth.setResizable(false);
-        //MainWindowAuth.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MainWindowAuth.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                ConnectionDB.ConnectionClose();
+                try {
+                    ConnectionDB.CloseDB();
+                } catch (SQLException ex) {
+                    System.out.println("Проблема с закрытием БД" + ex);
+                    System.exit(0);
+                }
                 System.exit(0);
             }
         });
-
         JPanel contents = new JPanel();
-        contents.setLayout(new GridLayout(4,2));
-        contents.add(new JLabel("Сервер запущен, база подключена"));
+        contents.setLayout(new GridLayout(3,1));
+        Font font = new Font("Century Gothic", Font.BOLD, 14);
+        JLabel label = new JLabel("Сервер запущен, база подключена");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setFont(font);
+        contents.add(label);
         JButton tableButton = new JButton("Показать таблицу");
+        tableButton.setFont(font);
         contents.add(tableButton);
         JButton closeButton = new JButton("Выключить сервер");
+        closeButton.setFont(font);
         contents.add(closeButton);
         MainWindowAuth.add(contents);
 
@@ -49,10 +54,14 @@ public class WindowServer {
 
         closeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                ConnectionDB.ConnectionClose();
+                try {
+                    ConnectionDB.CloseDB();
+                } catch (SQLException ex) {
+                    System.out.println("Проблема с закрытием БД" + ex);
+                    System.exit(0);
+                }
                 System.exit(0);
             }
         });
-
     }
 }
